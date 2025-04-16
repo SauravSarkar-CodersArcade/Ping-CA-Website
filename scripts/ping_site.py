83cfeb35e3338abd387ce2e8e2ca97aa18c25e7b
Saurav Sarkar <96565541+SauravSarkar-CodersArcade@users.noreply.github.com>
2025-04-16 23:55:14 +0530

    Updated ping_site.py to get time-stamp details

--- scripts/ping_site.py
+++ scripts/ping_site.py
@@ -1,9 +1,13 @@
 import requests
 import os
+import datetime
 
 URL = os.getenv("URL")  # Get URL from environment variable
+
+print(f"Running ping at: {datetime.datetime.utcnow()} UTC")
 try:
     response = requests.get(URL)
     print(f"Successfully pinged {URL}, Status Code: {response.status_code}")
 except Exception as e:
     print(f"Error pinging {URL}: {e}")
+
